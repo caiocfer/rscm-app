f72c092242d3b608ad3950893552ca05a1b5b196
Caio <caiocfer@gmail.com>
2022-08-30 01:34:42 -0300

    Added token related code
    This commit adds token related code, after login the token is saved in
    SharedPreferences.
    
    Signed-off-by: Caio <caiocfer@gmail.com>

---
+++ app/src/main/java/com/ferreiracaio/rscm_app/data/SessionManager.kt
@@ -0,0 +1,26 @@
+package com.ferreiracaio.rscm_app.data
+
+import android.content.Context
+import android.content.SharedPreferences
+import com.ferreiracaio.rscm_app.R
+
+class SessionManager(context: Context) {
+    private var prefs: SharedPreferences = context.getSharedPreferences(context.getString(R.string.app_name), 0)
+
+    companion object{
+        const val USER_TOKEN = "user_token"
+    }
+
+    fun saveAuthToken(token: String){
+        val editor = prefs.edit()
+        editor.putString(USER_TOKEN, token)
+        editor.apply()
+        editor.commit()
+    }
+
+    fun fetchAuthToken(): String?{
+        return prefs.getString(USER_TOKEN, null)
+    }
+
+
+}
--- app/src/main/java/com/ferreiracaio/rscm_app/presentation/login/LoginViewModel.kt
+++ app/src/main/java/com/ferreiracaio/rscm_app/presentation/login/LoginViewModel.kt
@@ -1,23 +1,25 @@
 package com.ferreiracaio.rscm_app.presentation.login
 
-import android.util.Log
+import android.content.Context
 import androidx.lifecycle.ViewModel
 import com.ferreiracaio.rscm_app.data.ApiService
+import com.ferreiracaio.rscm_app.data.SessionManager
 import com.ferreiracaio.rscm_app.models.LoginUser
 import okhttp3.ResponseBody
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
-class LoginViewModel : ViewModel(){
+class LoginViewModel() : ViewModel(){
 
-    fun loginUser(email: String,password: String){
+    fun loginUser(email: String,password: String, context:Context){
 
         ApiService.service.loginUser(LoginUser(email,password)).enqueue(object :
             Callback<ResponseBody>{
             override fun onResponse(call: Call<ResponseBody>, response: Response<ResponseBody>) {
                 if (response.code() == 200){
-                    Log.d("TAG", "onResponse: ${response.body()?.string()}")
+                    val token = response.body()?.string().toString()
+                    saveToken(token, context)
                 }
             }
 
@@ -26,8 +28,10 @@ class LoginViewModel : ViewModel(){
             }
 
         })
-
     }
 
-
+    private fun saveToken(token: String,context: Context){
+        val sessionManager = SessionManager(context)
+        sessionManager.saveAuthToken(token)
+    }
 }
