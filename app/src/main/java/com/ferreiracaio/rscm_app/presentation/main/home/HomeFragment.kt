09c9749850c69386b1a484727659d53776a74053
Caio <caiocfer@gmail.com>
2022-09-07 21:57:00 -0300

    Added HomeViewModel for the HomeFragment
    This commits adds the ViewModel that is going to be used in the
    HomeFragment.
    
    Signed-off-by: Caio <caiocfer@gmail.com>

--- app/src/main/java/com/ferreiracaio/rscm_app/presentation/main/home/HomeFragment.kt
+++ app/src/main/java/com/ferreiracaio/rscm_app/presentation/main/home/HomeFragment.kt
@@ -5,14 +5,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import com.ferreiracaio.rscm_app.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
 
     private var _binding: FragmentHomeBinding? = null
+    private lateinit var viewModel: HomeViewModel
 
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
 
     override fun onCreateView(
@@ -27,7 +27,7 @@ class HomeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+        viewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
     }
 
     override fun onDestroyView() {
---
+++ app/src/main/java/com/ferreiracaio/rscm_app/presentation/main/home/HomeViewModel.kt
@@ -0,0 +1,7 @@
+package com.ferreiracaio.rscm_app.presentation.main.home
+
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel:ViewModel() {
+
+}
